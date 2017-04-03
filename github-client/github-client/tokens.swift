72a28a5ae11730c96a876c973c97b99484575ba4
mikemiksch <mikemiksch@gmail.com>
2017-04-03 14:43:14 -0700

    Lecture done, but getting inexplicable 404 error.

---
+++ github-client/github-client/tokens.swift
@@ -0,0 +1,12 @@
+//
+//  tokens.swift
+//  github-client
+//
+//  Created by Mike Miksch on 4/3/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+import Foundation
+
+let gitHubClientID = "bdd4db46f6b9b712c58"
+let gitHubClientSecret = "e58fffcfb905f96cd798e0269631331d53d1b04b"
